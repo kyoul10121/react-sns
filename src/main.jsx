16384173cacf974411f17e489ca7efb100787a70
kyoul10121 <kyoul10121@naver.com>
2024-09-08 18:26:19 +0900

    Style: emotion-normalize 및 글로벌 스타일 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import GlobalStyles from './styles/GlobalStyles.jsx';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <>
+      <GlobalStyles />
+      <App />
+    </>
   </StrictMode>
 );
---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,33 @@
+/** @jsxImportSource @emotion/react */
+import { Global, css } from '@emotion/react';
+import emotionNormalize from 'emotion-normalize';
+
+const GlobalStyles = () => {
+  return (
+    <Global
+      styles={css`
+        ${emotionNormalize}
+
+        @font-face {
+          font-family: 'Pretendard-Regular';
+          src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+            format('woff');
+          font-weight: 400;
+          font-style: normal;
+        }
+
+        html,
+        body {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          background: white;
+          min-height: 100%;
+          font-family: 'Pretendard-Regular';
+        }
+      `}
+    />
+  );
+};
+
+export default GlobalStyles;
