d0c2d2538ee58c492fe5df8ab57d4df9d7fab432
kyoul10121 <kyoul10121@naver.com>
2024-09-18 20:34:43 +0900

    fix: json 파일 경로 오류 수정, resolveJsonModule 옵션 추가

--- src/components/MessageWindow.tsx
+++ src/components/MessageWindow.tsx
@@ -3,9 +3,8 @@ import { css } from '@emotion/react';
 import { useState, useEffect, useRef } from 'react';
 import MessageList from './MessageList';
 import MessageInput from './MessageInput';
-import chatData from '/public/chatData.json';
-import userData from '/public/userData.json';
-
+import chatData from '../db/chatData.json';
+import userData from '../db/userData.json';
 const MessageWindow = () => {
   // 메세지 입력창을 포함한 모든 대화창
 
